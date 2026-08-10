72898d86cedd9a9c00aec37817cccdd3f5c61813
Hari Kumar <harikumar.codes@gmail.com>
2026-08-10 12:55:03 +0100

    rename playlist

--- src/Playlist/Playlist.jsx
+++ src/Playlist/Playlist.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react'
 import Tracklist from '../Tracklist/Tracklist'
 
 function Playlist({ tracks, onRemoveTrack }) {
+  const [playlistName, setPlaylistName] = useState('')
+
+  const handlePlaylistNameChange = e => {
+    setPlaylistName(e.target.value)
+  }
+
   return (
     <>
       <h2>Playlist</h2>
-      <input type="text" placeholder="Playlist Name" />
+      <input
+         type="text"
+         placeholder="Playlist Name"
+         value={playlistName}
+         onChange={handlePlaylistNameChange}
+       />
       <Tracklist tracks={tracks} isAdded={true} onTrackAction={onRemoveTrack} />
       <button>SAVE TO SPOTIFY</button>
     </>
