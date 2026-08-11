ed859f1a9e9b6e044147b08e4361b5ada8b76c44
Hari Kumar <harikumar.codes@gmail.com>
2026-08-12 00:17:07 +0100

    add track layout styles

--- src/Track/Track.jsx
+++ src/Track/Track.jsx
@@ -1,12 +1,16 @@
+import styles from './Track.module.css'
+
 function Track({ track, isAdded, onAction }) {
   return (
-    <>
-      <h3>{track.name}</h3>
-      <p>{track.artist} | {track.album}</p>
-      <button onClick={() => onAction(track)}>
+    <section className={styles.track}>
+      <div className={styles.details}>
+        <h3>{track.name}</h3>
+        <p>{track.artist} | {track.album}</p>
+      </div>
+      <button className={styles.button} onClick={() => onAction(track)}>
         {isAdded ? '-' : '+'}
       </button>
-    </>
+    </section>
   )
 }
 
